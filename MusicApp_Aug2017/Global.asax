9c5093ff634b11f0674bd66e76911f0b3dd3e836
Jonas <jkpaskus@gmail.com>
2017-08-11 16:38:08 -0400

    First commit

---
+++ MusicApp_Aug2017/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MusicApp_Aug2017.MvcApplication" Language="C#" %>
---
+++ MusicApp_Aug2017/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MusicApp_Aug2017.Startup))]
+namespace MusicApp_Aug2017
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
