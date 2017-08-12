2b64f570d91a8e40486397ac459dd5609f51b793
Jonas <jkpaskus@gmail.com>
2017-08-11 21:59:33 -0400

    Second commit, adding on to database

---
+++ MusicApp_Aug2017/Models/Gig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MusicApp_Aug2017.Models
+{
+    public class Gig
+    {
+        //Properties we need for our Gig class.
+        public int ID { get; set; }
+
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+    }
+
+   
+}
